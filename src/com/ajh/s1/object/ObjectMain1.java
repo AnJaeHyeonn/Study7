3738d3a2af3eb73b59eced51dcf7ae6a24e4cbac
AnJaeHyeonn <ahn9801@naver.com>
2021-07-23 17:14:08 +0900

    7.23

--- src/com/ajh/s1/object/ObjectMain1.java
+++ src/com/ajh/s1/object/ObjectMain1.java
@@ -1,5 +1,26 @@
 package com.ajh.s1.object;
 
 public class ObjectMain1 {
+	public static void main(String[] args) {
+		// 객체 생성
+		// 클래스명 참조변수명 = new 생성자();
+		// Object 객체 생성
+		Object obj = new Object();
+
+		// 멤버변수, 멤버메서드 호출
+		// 참조변수명.변수명
+		// 참조변수명.메서드명([인자값]);
+		int num = obj.hashCode();
+		System.out.println(num);
+
+		String str = obj.toString();
+		System.out.println(str);
+		System.out.println(obj);
+		
+		Object obj2 = new Object();
+
+		boolean result = obj.equals(obj2);
+		System.out.println(result);
+	}
 
 }
