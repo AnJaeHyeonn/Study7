aa732c4c8d49afb864fad4caa20c0aea083d3e49
AnJaeHyeonn <ahn9801@naver.com>
2021-08-02 17:35:30 +0900

    8.02

---
+++ src/com/ajh/s6/thread/Test3.java
@@ -0,0 +1,12 @@
+package com.ajh.s6.thread;
+
+public class Test3 implements Runnable {
+
+	@Override
+	public void run() {
+		for (int i = 'A'; i <= 'Z'; i++) {
+			System.out.println((char) i);
+		}
+	}
+
+}
--- src/com/ajh/s6/thread/TestMain.java
+++ src/com/ajh/s6/thread/TestMain.java
@@ -4,9 +4,13 @@ public class TestMain {
 	public static void main(String[] args) {
 		Test1 t1 = new Test1();
 		Test2 t2 = new Test2();
+		Test3 t3 = new Test3();
+		Thread thread = new Thread(t3);
 
-		t1.run();
-		t2.run();
+		t1.start();
+		t2.start();
+		thread.start();
+
 	}
 
 }
